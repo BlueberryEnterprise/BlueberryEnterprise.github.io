dde8ca7bc5b36d869a986554685709d13474669e
BlueberryEnterprise <78371406+BlueberryEnterprise@users.noreply.github.com>
2021-04-18 12:57:15 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const target = document.querySelectorAll('[data-color]');
 const colorClass = 'colorNav'
 
+
 function colorNav(){
     const windowTop = window.scrollY
     target.forEach((e)=>{
@@ -16,3 +17,18 @@ function colorNav(){
 window.addEventListener('scroll', ()=>{
     colorNav()
 })
+
+function sizeOfThings(){
+    var windowWidth = window.innerWidth;
+    let navbar = document.querySelectorAll('.navbar-nav')
+    navbar.forEach((e)=>{
+        if(windowWidth < 992){
+            e.classList.add(colorClass)
+        }else{
+            e.classList.remove(colorClass)
+        }
+    })
+    
+};
+window.addEventListener('resize', sizeOfThings)
+sizeOfThings();
